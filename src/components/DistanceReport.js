de83e534490f881816d542a4b0c054a2c751df87
riyazuddin <md.riyazuddeen@gmail.com>
2018-03-08 12:00:45 +0530

    integrated distance report with sample data

--- src/components/DistanceReport.js
+++ src/components/DistanceReport.js
@@ -31,17 +31,18 @@ export default class GPSTrackLocation extends Component {
         fromPassdate:'',
         toDate:'',
         toPassdate:'',
-       
-          spinnerBool: false,
-
+        spinnerBool: false,
+        records:[{_id:1,trucknum:'TS36AA5432',localArea:'madhapur',City:'Hyderabad',dist:'542.00KMS'},
+              {_id:2,trucknum:'0D96AA1532',localArea:'madhapur',City:'Ordisha',dist:'632.00KMS'},
+              {_id:3,trucknum:'AP52AA9532',localArea:'Vishka',City:'Vijayawad',dist:'258.00KMS'},
+              {_id:4,trucknum:'CH87AA6332',localArea:'Otty',City:'Hyderabad',dist:'784.00KMS'}]
     };
 
     componentWillMount() {
-        console.log('this.props gpstractlocation',this.props.nav)
+        console.log('this.props distanceReport',this.props.nav)
         const self = this;
-        console.log(self.props,"GPSTrackLocation=token");
-        this.getCredentailsData();
-        self.setState({showDependable:'0'});
+        console.log(self.props,"distanceReport=token");
+       // this.getCredentailsData();
     }
         componentWillUnmount(){
          BackHandler.removeEventListener('hardwareBackPress', this.onBackAndroid.bind(this));
@@ -133,7 +134,7 @@ export default class GPSTrackLocation extends Component {
       renderSeparator = () => (
         <View
           style={{
-            backgroundColor: '#d6d6d6',
+            backgroundColor: 'transparent',
             height: 1,
           }}
         />
@@ -320,52 +321,51 @@ export default class GPSTrackLocation extends Component {
                                 </View>
 
                         </View>
+                        <View style={{ flex:1,alignSelf:'stretch',flexDirection: 'column',paddingTop:5,justifyContent:'space-between',
+                                backgroundColor:'#f8f8f8',width:'100%',paddingHorizontal:10,paddingTop:10,
+                                height:50}}>
 
-                        {this.spinnerLoad()}
-                        <FlatList 
-                        data={this.state.coordinates1}
+                               
+                       {console.log(this.state.records)}
+                        <FlatList  style={{backgroundColor:'#ffffff',flex:1}}
+                        data={this.state.records}
                         ItemSeparatorComponent={this.renderSeparator}
                         removeClippedSubviews ={true}
                         renderItem={({ item }) =>{
-                        if(item.isStopped){
+                            {console.log(item)}
                            return( 
-                           <View style={[CustomStyles.erpCategoryItems, { backgroundColor: !this.state.categoryBgColor ? '#ffffff' : '#f6f6f6' }]}>
+                               
+                           <View style={[CustomStyles.erpCategoryItems, { backgroundColor:'#ffffff' }]}>
                                 <View style={CustomStyles.erpDriverItems}>
-                                    <View style={[CustomStyles.erpTextView, { flex: 0.6, borderBottomWidth: 0 }]}>
-                                        <Image resizeMode="contain"
-                                            source={require('../images/truck_stops.png')}
-                                            style={CustomStyles.imageViewContainer} />
-                                        
-                                    </View>
+                                    
                                     <View style={{ flex: 1, flexDirection: 'column', padding: 10}}>
-
-                                        <View style={{ flex: 1, flexDirection: 'row', padding: 10 }}>
-                                            <View style={{ flex: 1, flexDirection: 'column', padding: 10 }}>
-                                                <Text style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 16, }]}>
-                                                    Date</Text>
-                                                <Text style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 16, }]}>
-                                                {this.getParsedDate(item.updatedAt)}</Text>
+                                        <View style={{ flex: 1, flexDirection: 'row', padding: 5 }}>
+                                            <View style={{ flex: 1, flexDirection: 'column', padding: 2 }}>                                                
+                                                <Text numberOfLines={1} style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 12, }]}>
+                                                {item.trucknum}</Text>
                                             </View>
-                                            <View style={{ flex: 1, flexDirection: 'column', padding: 10 }}>
-                                                <Text style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 16, }]}>
-                                                    Time</Text>
-                                                <Text style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 16, }]}>
-                                                { this.getParsedtime(item.updatedAt)} {}</Text>
+                                            <View style={{ flex: 1, flexDirection: 'column', padding: 2 }}>                                                
+                                                <Text numberOfLines={1}style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 12, }]}>
+                                                { item.localArea} </Text>
+                                            </View>
+                                            <View style={{ flex: 1, flexDirection: 'column', padding: 2 }}>                                                
+                                                <Text numberOfLines={3}  style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 12, }]}>
+                                                { item.City} </Text>
+                                            </View>
+                                            <View style={{ flex: 1, flexDirection: 'column', padding: 2 }}>                                                
+                                                <Text numberOfLines={1}style={[CustomStyles.erpText, { fontFamily: 'Gotham-Medium', fontSize: 12, }]}>
+                                                { item.dist} </Text>
                                             </View>
                                         </View>
                                     </View>
                                 </View>
-                            </View>)}
-                            // }else{
-                            //     return(
-                            //         <View style={{height:10,backgroundColor:'orange'}}>
-                            //         </View>
-                            //     ) 
-                            // }
+                            </View>)
                         }
                     }
                     keyExtractor={item => item._id} />
-                        
+
+                </View>
+                         {this.spinnerLoad()}
                 </View>
                 
             );           
